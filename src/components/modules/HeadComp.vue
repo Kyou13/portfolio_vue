debcd1e1a81d5b28038bac9971aba9722992a1b2
Kyou13 <kyou1013@gmail.com>
2019-02-24 11:56:15 +0900

    first commit

---
+++ src/components/modules/HeadComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/works">Works</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'headComp'
+  }
+</script>
